ddc2b9c21b1e22b555970a77608c50c8dd2b6795
hafizatif07 <hafiz.mahmood@accenture.com>
2025-10-28 13:33:28 +0000

    apply another fix to resolve the path

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from djongo import models
+from bson import ObjectId
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
@@ -13,15 +14,17 @@ class User(AbstractUser):
         related_name='octofit_user_permissions',
         blank=True
     )
+    class Meta:
+        app_label = 'octofit_tracker'
 
 class Team(models.Model):
-    id = models.ObjectIdField(primary_key=True, editable=False)
+    id = models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     name = models.CharField(max_length=100, unique=True)
     class Meta:
         app_label = 'octofit_tracker'
 
 class Activity(models.Model):
-    id = models.ObjectIdField(primary_key=True, editable=False)
+    id = models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     name = models.CharField(max_length=100)
     user_email = models.CharField(max_length=100)
     team = models.CharField(max_length=100)
@@ -29,14 +32,14 @@ class Activity(models.Model):
         app_label = 'octofit_tracker'
 
 class Leaderboard(models.Model):
-    id = models.ObjectIdField(primary_key=True, editable=False)
+    id = models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     team = models.CharField(max_length=100)
     points = models.IntegerField()
     class Meta:
         app_label = 'octofit_tracker'
 
 class Workout(models.Model):
-    id = models.ObjectIdField(primary_key=True, editable=False)
+    id = models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     name = models.CharField(max_length=100)
     description = models.TextField()
     class Meta:
